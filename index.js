30865bcb09f947adbc72433b1512262ae239a021
Roberto Poblete Tapia <rnm.crea@gmail.com>
2025-09-17 23:20:50 -0300

    Actualizar index.js

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const nodemailer = require('nodemailer');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
 
 app.use(cors());
 app.use(express.json());
